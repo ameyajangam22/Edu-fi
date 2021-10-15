763c766c073c454eaf925ba59ee440cdf22fb4b2
sahil27301 <65967824+sahil27301@users.noreply.github.com>
2021-10-16 01:02:45 +0530

    google oauth setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+require('./config/db')();
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const app = express();
-var cors = require('cors');
+
+// passport config
+require('./config/passport')(passport);
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  }),
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //body parser
 app.use(express.json());
@@ -9,7 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 // To prevent cors error
 app.use(cors());
 
+app.use('/auth', require('./routes/auth'));
+
 //listen
 app.listen(8000, () => {
-    console.log('Server up and running');
+  console.log('Server up and running');
 });
