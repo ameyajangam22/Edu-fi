5a4bdf9aff9a524d8b238affe4c93424afdd97d6
Atharva Mohite <atharvamohite20@gmail.com>
2021-10-18 13:41:35 +0530

    user controller done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,8 @@ const User = require('../models/User');
 
 const getAllTeachers = async (req, res) => {
   try {
-    res.status(200).json({ data: 'All users' });
+    const teachers = await User.find({ isTeacher: true });
+    res.status(200).json({ data: teachers });
   } catch (error) {
     res.status(500).json({ error });
   }
@@ -10,7 +11,8 @@ const getAllTeachers = async (req, res) => {
 
 const getAllStudents = async (req, res) => {
   try {
-    res.status(200).json({ data: 'All users' });
+    const students = await User.find({ isTeacher: false });
+    res.status(200).json({ data: students });
   } catch (error) {
     res.status(500).json({ error });
   }
